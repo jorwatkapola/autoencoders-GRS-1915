cb00b30f0e4a441a65ab7974f7109ce15fce6f1b
Jakub Orwat-Kapola <jkok1g14@soton.ac.uk>
2021-09-10 12:09:18 +0100

    added a function to verify that all compatible light curves were segmented

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,4 +1,27 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from termcolor import colored
+
+
+
+def simple_segmentation(time_series, segment_length, stride, keep_time_stamps, input_cadence_sec):
+    
+    segments = []
+    for start in range(0, int(len(time_series[0])-segment_length), stride): 
+        end=start+segment_length
+        # test that the segment has the expected duration in seconds
+        # if it doesn't then the segment likely goes outside the GTI
+        new_segment_time = time_series[0][start:end+1]
+        new_segment_duration_sec = new_segment_time[-1]-new_segment_time[0]
+        if new_segment_duration_sec != segment_length*input_cadence_sec: #don't allow segments outside of good time intervals
+            print("segment outside of good time intervals")
+            continue
+        if keep_time_stamps==True:
+            segments.append(time_series[:,start:end])
+        else:
+            segments.append(time_series[1:,start:end])
+            
+    return np.array(segments)
 
 def segmentation(time_series, segment_length_sec, stride_sec, keep_time_stamps=True, input_cadence_sec=4):
     """
@@ -9,19 +32,93 @@ def segmentation(time_series, segment_length_sec, stride_sec, keep_time_stamps=T
     seg_len = length of a segment, 
     stride_sec = step size; difference in the starting position of the consecutive segments
     """
-    segment_length = segment_length_sec//input_cadence_sec
-    stride = stride_sec//input_cadence_sec
+    segment_length = int(segment_length_sec//input_cadence_sec)
+    stride = int(stride_sec//input_cadence_sec)
     
-    segments=[]
-    for start in range(0, len(time_series[0])-segment_length, stride):
-        end=start+segment_length
-        if time_series[0][end]-time_series[0][start] != segment_length*input_cadence_sec: #don't allow segments outside of good time intervals
-            continue
-        if keep_time_stamps==True:
-            segments.append(time_series[:,start:end])
-        else:
-            segments.append(time_series[1:,start:end])
-    return np.array(segments)
+    time_deltas = np.diff(time_series[0])
+    gaps = np.where(time_deltas>input_cadence_sec)[0]
+    
+    segments_all=[]
+
+    if len(gaps) == 0:
+        segments_all = simple_segmentation(time_series, segment_length, stride, keep_time_stamps, input_cadence_sec)
+                
+    else:
+        for gap_ind, gap in enumerate(gaps):
+            if gap_ind == 0:
+                segments_all.append(simple_segmentation(time_series[:,:gap+1], segment_length, stride, keep_time_stamps, input_cadence_sec))
+                
+            else:
+                segments_all.append(simple_segmentation(time_series[:,gaps[gap_ind-1]+1:gap+1], segment_length, stride, keep_time_stamps, input_cadence_sec))
+                
+            if gap == gaps[-1]:
+                segments_all.append(simple_segmentation(time_series[:,gap+1:], segment_length, stride, keep_time_stamps, input_cadence_sec))
+                
+        
+        segments_all = [item for sublist in segments_all for item in sublist] # vstack the list of lists of data segments
+    
+    return np.array(segments_all)
+
+def verify_not_segmented_light_curves(lcs, ob_state, seg_ids, lc_ids, seg_len_sec=1024):
+    """
+    Plot and show GTI length of the light curves which were not segmented
+    
+    Parameters:
+    --------------
+    lcs: list of ndarrays 
+        ndarrays of shape (3,light_curve_length) containing time, count rate and count rate error data
+        
+    ob_state: dict
+        Classifications of light curves in the form {observation_ID: class_label}
+        
+    seg_ids: list of strings
+        identifiers for segment light curves in the form of  "{}_{}".format(observation_ID, integer)
+    
+    lc_ids: list of strings
+        identifiers for light curvesin the for of observation_ID, e.g. "10408-01-17-03"
+    
+    """
+    rejected_lcs_with_label = 0
+    for k,v in ob_state.items():
+        if k not in [x.split("_")[0] for x in seg_ids]:
+            print(k, v)
+            try:
+                n = np.where(np.array(lc_ids) == k)[0][0]
+                plt.plot(lcs[n][0], lcs[n][1])
+                plt.show()
+                cadence = np.min(np.diff(lcs[n][0]))
+                print("sum of GTI durations in seconds: ", (len(lcs[n][0])-1)*cadence)
+                time_deltas = np.diff(lcs[n][0])
+                gaps = np.where(time_deltas>cadence)[0]
+                if len(gaps)>0:
+                    print("individual GTI durations in seconds:")
+                    for gap_ind, gap in enumerate(gaps):
+                        if gap_ind == 0:
+                            if gap*cadence >= seg_len_sec:
+                                print(gap*cadence, colored("GTI seems long enough for segmentation", "red"))
+                            else:
+                                print(gap*cadence)
+                        else:
+                            if (gap-(gaps[gap_ind-1]+1))*cadence >= seg_len_sec:
+                                print((gap-(gaps[gap_ind-1]+1))*cadence, colored("GTI seems long enough for segmentation", "red"))
+                            else:
+                                print((gap-(gaps[gap_ind-1]+1))*cadence)
+
+                        if gap == gaps[-1]:
+                            if (len(lcs[n][0])-(gap+1))*cadence >= seg_len_sec:
+                                print((len(lcs[n][0])-(gap+1))*cadence, colored("GTI seems long enough for segmentation", "red"))
+                            else:
+                                print((len(lcs[n][0])-(gap+1))*cadence)
+                            
+                elif (len(lcs[n][0])-1)*cadence >= seg_len_sec:
+                    print(colored("Light curve seems long enough for segmentation", "red"))
+                    
+            except:
+                print("No data")
+            rejected_lcs_with_label+=1
+            print("\n\n")
+    print(rejected_lcs_with_label)
+
 
 def binning(time_series, output_cadence, input_cadence=0.125):
     """
